8efef7c09be6159dacb43f35be8b6a374e7ab098
Oleksandr Hvozdyk <oleksandr.hvozdyk@gmail.com>
2019-04-13 23:30:08 +0300

    feature: add sfc

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
     <h1>Event List</h1>
-    <router-link :to="{ name: 'event-show', params: { id: '1' } }"
-      >Show Event #1</router-link
-    >
+    <EventCard />
   </div>
 </template>
+<script>
+import EventCard from '@/components/EventCard.vue'
+export default {
+  components: { EventCard }
+}
+</script>
