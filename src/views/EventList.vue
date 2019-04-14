04e30698b059b03149a4611506110d9eaa70df69
Oleksandr Hvozdyk <oleksandr.hvozdyk@gmail.com>
2019-04-14 22:28:00 +0300

    feature: axios' API calls

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,12 +1,25 @@
 <template>
   <div>
     <h1>Event List</h1>
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
+
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+
 export default {
-  components: { EventCard }
+  data() {
+    return {
+      events: []
+    }
+  },
+  components: { EventCard },
+  created() {
+    EventService.getEvents()
+      .then(res => (this.events = res.data))
+      .catch(err => console.log(`There was an error: ${err.response}`))
+  }
 }
 </script>
