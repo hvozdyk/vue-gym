213cdb3a47f943747998f38028358dbca5db48f0
Oleksandr Hvozdyk <oleksandr.hvozdyk@gmail.com>
2019-04-17 12:40:08 +0300

    feature: mutations and actions
    - fetch events with vuex
    - add pagination to event list
    - fetch event with vuex

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,24 +2,53 @@
   <div>
     <h1>Event List</h1>
     <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <template v-if="page !== 1">
+        <router-link
+          :to="{ name: 'event-list', query: { page: page - 1 } }"
+          rel="next"
+          >Prev</router-link
+        >
+      </template>
+      <template v-if="eventsTotal > page * 3">
+        <router-link
+          :to="{ name: 'event-list', query: { page: page + 1 } }"
+          rel="prev"
+          >Next</router-link
+        >
+      </template>
+    </div>
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
-import EventService from '@/services/EventService.js'
+import { mapState } from 'vuex'
 
 export default {
-  data() {
-    return {
-      events: []
-    }
-  },
   components: { EventCard },
   created() {
-    EventService.getEvents()
-      .then(res => (this.events = res.data))
-      .catch(err => console.log(`There was an error: ${err.response}`))
+    this.perPage = 3
+    this.$store.dispatch('fetchEvents', {
+      perPage: this.perPage,
+      page: this.page
+    })
+  },
+  computed: {
+    page() {
+      return parseInt(this.$route.query.page) || 1
+    },
+    ...mapState(['events', 'eventsTotal'])
   }
 }
 </script>
+
+<style scoped>
+.pagination {
+  display: flex;
+  justify-content: center;
+}
+a {
+  margin: 0 10px;
+}
+</style>
