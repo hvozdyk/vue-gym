9021f554f9b38ee802a7c00c047fbe7527845fcd
Oleksandr Hvozdyk <oleksandr.hvozdyk@gmail.com>
2019-04-18 20:06:40 +0300

    Add progress bar to EventCreate

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -57,6 +57,7 @@
 
 <script>
 import Datepicker from 'vuejs-datepicker'
+import NProgress from 'nprogress'
 
 export default {
   components: {
@@ -75,13 +76,19 @@ export default {
   },
   methods: {
     createEvent() {
-      this.$store.dispatch('event/createEvent', this.event).then(() => {
-        this.$router.push({
-          name: 'event-show',
-          params: { id: this.event.id }
+      NProgress.start()
+      this.$store
+        .dispatch('event/createEvent', this.event)
+        .then(() => {
+          this.$router.push({
+            name: 'event-show',
+            params: { id: this.event.id }
+          })
+          this.event = this.createFreshEventObject()
         })
-        this.event = this.createFreshEventObject()
-      })
+        .catch(() => {
+          NProgress.done()
+        })
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user
